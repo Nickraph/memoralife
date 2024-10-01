34da951159688bf4bf758284bb77f3dcc3b04558
nick <nikosmozas@gmail.com>
2024-10-02 01:25:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -161,11 +161,11 @@ io.sockets.on('connection', function(socket){//SOCKETS++++++
 	});
 
 	socket.on("updateUserInfo", function(data){//called when user updates/edits profile info (later add email+pass editing)
-		for(i in informationColumns){
+		/*for(i in informationColumns){
 			if(data[informationColumns[i]] != ""){//maybe make it into a string then send query
 				client.query('INSERT INTO information('+informationColumns[i]+') VALUES($1)', [data[informationColumns[i]]]);
 			}
-		}
+		}*/
 	});
 
 	/*/create new account ID
