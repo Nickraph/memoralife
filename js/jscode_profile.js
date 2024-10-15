b56bb85735987008db3fe3882c32e53cf296084d
nick <nikosmozas@gmail.com>
2024-10-15 16:19:15 +0300

    Update jscode_profile.js

--- js/jscode_profile.js
+++ js/jscode_profile.js
@@ -12,6 +12,10 @@ document.addEventListener('DOMContentLoaded', () => {
     //Retrieve user information from localStorage
     info = JSON.parse(localStorage.getItem("userInfo"));
 
+    var informationColumns = [
+        "name", "surname", "dob", "pob", "nickname", "generalinfo", "address", "familynames", "familyoccupations", "pets", "childhoodinfo", "address_childhood", "school_childhood", "lovememories", "memories_childhood_misc", "media_childhood", "studies", "occupations", "marriage", "partnerinfo", "kids", "memories_adulthood_misc", "grandchildren", "media_seniority", "values", "achievements", "fav_foods", "fav_scents", "fav_fun", "fav_seasons", "fav_media", "fav_memories", "fav_music", "fav_hobbies", "fav_misc", "leastfav", "routine"
+    ];
+
     // Generate all memory (text & media) storing divs & category dividers dynamically.
     const memoryContainer = document.getElementById("memory-container");
 	const memoryFieldNames = [
@@ -34,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
         //Information inside
         const paragraph = document.createElement("p");
         paragraph.id = `divinfo${i}`;
-        paragraph.textContent = "Information Placeholder";
+        paragraph.textContent = info[informationColumns[i]];
 
         //Hidden edit buttons
         const editBtn = document.createElement("button");
