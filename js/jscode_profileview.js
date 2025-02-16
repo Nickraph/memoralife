d38ee704eda4b8caa920006794628678cad6b6a2
nick <nikosmozas@gmail.com>
2025-02-16 23:46:26 +0200

    .

--- js/jscode_profileview.js
+++ js/jscode_profileview.js
@@ -5,23 +5,10 @@ var info; // ! USE THIS VARIABLE INSTEAD OF LOCALSTORAGE. LOCALSTORAGE BECOMES O
 document.addEventListener('DOMContentLoaded', () => { 
     //Retrieve user information from localStorage
     info = JSON.parse(localStorage.getItem("userInfo"));
-    let token = localStorage.getItem("sessionToken");
-    localStorage.removeItem("sessionToken");
-    sessionStorage.setItem("sessionToken", token);
 
     var informationColumns = [
         "name", "surname", "dob", "pob", "nickname", "generalinfo", "address", "familynames", "familyoccupations", "pets", "childhoodinfo", "address_childhood", "school_childhood", "lovememories", "memories_childhood_misc", "media_childhood", "studies", "occupations", "marriage", "partnerinfo", "kids", "memories_adulthood_misc", "media_adulthood", "grandchildren", "media_seniority", "values", "achievements", "fav_foods", "fav_scents", "fav_fun", "fav_seasons", "fav_media", "fav_memories", "fav_music", "fav_hobbies", "fav_misc", "leastfav", "routine", "media_misc"
     ];
-
-    //add settings details
-    document.getElementById("handleSpan").innerHTML = "Αναγνωριστηκό: "+info.handle;
-    document.getElementById("currentEmail").innerHTML = "Τρέχον email: "+info.email;
-    if(info.visibility == "private"){
-        document.getElementById('radio_private').checked = true;
-    }
-    else{
-        document.getElementById('radio_visible').checked = true;
-    }
 
     // Generate all memory (text & media) storing divs & category dividers dynamically.
     const memoryContainer = document.getElementById("memory-container");
@@ -168,23 +155,8 @@ document.addEventListener('DOMContentLoaded', () => {
         pfpImg.src = info.pfp;
     }
 
-    //add prompt to guide and/or questionnaire
-    if(info.init === "not_init"){
-        document.getElementById("initModal").style.display = "flex"; // Display modal
-
-        //add show class to initModal-Btn after 2 seconds
-        setTimeout(function() {
-            document.getElementById("initModal-Btn").classList.add("show");
-        }, 2000);
-
-        let sessionToken = sessionStorage.getItem("sessionToken");
-        let data_name = "init";
-        let data_value = "init";
-        let updatePacket = {data_name, data_value, sessionToken};
-        socket.emit("updateCredentials", updatePacket);
-    }
-
     let currentIndex = 0; // local variable for information input
+
     // Enter information in their fields
     informationColumns.forEach((data_name, index) => {
         
